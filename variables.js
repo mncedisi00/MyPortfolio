7131db92c791310967ea1385ae8cbb3cfad83be8
mncedisi <mncedisi.m2409@gmail.com>
2024-04-23 11:51:46 +0200

    java script

---
+++ variables.js
@@ -0,0 +1,34 @@
+const name = Mcedisi;
+const surname = Mhlanga;
+
+const educBackgrnd = "My name is Mncedisi Mhlanga and I am 23 years old." +
+"I was born and raised in Alexandra where I also attended" +
+ "my primary school called Carter."
+"There I got a bursary from the Alexandra Education Committee(AEC)"+
+"on the year 2014 when I was in grade 7." +
+"I then went to a private high school called Vuleka Sekolo Sa"+ 
+"Borokgo(SSB) that my bursary paid for."+
+"After matric I was at the University of Johannesburg pursuing "+
+"a BSc in Mathematical Sciences in Mathematics and Computer Science."+ 
+"Unfortunately this year I was unable to register due to financial"+
+ "constraints.<br> I have a certicate of Programming Fundamentals Using Python that I got"
+"last year from BCIT(British Columbia Internatinal Technology)."+
+"On my second year at UJ I also did the C++ language and computer competence.<br> I am wholly "+
+"committed to build a career in Software Development thus I am open to any "+
+"opportunities to grow my knowledge and applying it in a dynamic environment.";
+
+const bio ="I am a young adult at the age of 23 this year."+
+ "I was born and raised in Alexandra township where I also attended at Carter Primary School from the+ year 2008 - 2014."
+ "I then got a bursary from the Alexandra Education Committee(AEC) to attend a private high school in Randburg called"+
+"Vuleka Sekolo Sa Borokgo(SSB) from 2014 - 2019. After matric I was enrolled for a BSc in Mathematical Science in Mathematics and Computer Science"+
+"at the University of Johannesburg but I could not finish my 3rd year due to financial constraints.";
+const address = "8th avenue"+
+                "Alexandra"+
+                "Johannesburg"+
+                "2090";
+
+const province = "Gauteng";
+const city = "Johannesburg";
+const email = "Email Address:";
+const message = "Message:";
+const submitButton = "Submit";
